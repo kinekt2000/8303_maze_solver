572d7031c3befeb8e6c372e6ecada9817c376cc3
Constantine <kinekt2000@gmail.com>
2020-07-05 22:15:54 +0300

    stepBack function and appropriate button are connected

--- src/ConcreteListeners/AlgorithmListener.java
+++ src/ConcreteListeners/AlgorithmListener.java
@@ -24,7 +24,7 @@ public class AlgorithmListener implements Listener{
         }
 
         if(function.equals("back")) {
-            // return to saved copy
+            target.stepBack();
         }
 
         if(function.equals("forward")) {
