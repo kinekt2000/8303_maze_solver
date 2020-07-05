83d826025d56fbba1d5a6c00c38660408f04fcbe
Constantine <kinekt2000@gmail.com>
2020-07-05 18:27:20 +0300

    ui events and data conversion are conneted. But it still doesn't work :(

--- src/ConcreteDialogs/OpenFileDialog.java
+++ src/ConcreteDialogs/OpenFileDialog.java
@@ -1,10 +1,38 @@
 package ConcreteDialogs;
 
+import DrawableModel.TileMap;
+
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
 public class OpenFileDialog extends FileDialog{
     final public String name = "open_file";
+
+    TileMap target;
+
+    public OpenFileDialog(TileMap target) {
+        this.target = target;
+    }
 
     @Override
     public String getName() {
         return name;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        super.mouseClicked(e);
+
+        if(accepted) {
+            if(target != null && chosen.getLine().length() > 0) {
+                try {
+                    target.load("saves/" + chosen.getLine() + ".pfsv");
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } catch (ClassNotFoundException classNotFoundException) {
+                    classNotFoundException.printStackTrace();
+                }
+            }
+        }
     }
 }
